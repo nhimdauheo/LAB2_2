4dd6a98dec46874c02cdf36286a74b4977b59099
nhimdauheo <hieunguyen479@gmail.com>
2022-01-19 11:03:36 +0700

    Configuring React

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Navbar, NavbarBrand } from 'reactstrap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar dark color="warning">
+        <div className='container'>
+          <NavbarBrand href='/'>Hôm nay trời đẹp thế nhờ ỏoooooooooooooooooooooo</NavbarBrand>
+        </div>
+      </Navbar>
+    </div>
+  );
+}
+export default App;
